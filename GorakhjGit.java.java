58357070d43a5825ec573ba537a7816da7f75d5b
gorakh2021 <97584812+gorakh2021@users.noreply.github.com>
2022-01-13 00:24:28 +0530

    Add files via upload

---
+++ GorakhjGit.java.java
@@ -0,0 +1,20 @@
+package forloop;
+
+public class Textforloop {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=100; i++)
+		{
+		  System.out.println(i);
+		  
+		  if(i==50)
+		  {
+			  break;
+		  }
+		  
+		}
+		
+	}
+
+}
